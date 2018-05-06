b4fc8f1a39afe7fda91dcf0bbe55fb230968821e
Nick Sells <adverseengineer@gmail.com>
2018-05-06 01:45:02 -0400

    perfect the dice and uptime commands

--- cmds.js
+++ cmds.js
@@ -1,129 +1,213 @@
 exports.roll = function (arg)
 {
-	var pattern = new RegExp("([1-9]\\d*)?d([1-9]\\d*)([/x][1-9]\\d*)?([+-]\\d+)?");
-	var matches = arg.toLowerCase().substring(6,arg.length+1).match(pattern);
-	return matches.length;
+	var pattern = /([0-9])\w+/g;
+	var matches = arg.toLowerCase().substring(6, arg.length + 1).match(pattern);
+
+	var finalMessage = '';
+	var finalSum = 0;
+
+	for(var i = 0; i < matches.length; i++)
+	{
+		var numberOfRolls = parseInt(matches[i].substring(0,matches[i].indexOf('d')));
+		var numberOfSides = parseInt(matches[i].substring(matches[i].indexOf('d')+1, matches[i].length+1));
+		var termSum = 0;
+		var thisTermMessage = '';
+		thisTermMessage += matches[i] + ' (';
+		for(var j = 0; j < numberOfRolls; j++)
+		{
+			var thisRoll = Math.floor(Math.random() * (numberOfSides - 1)) + 1;
+			termSum += thisRoll;
+			thisTermMessage += thisRoll.toString();
+			if(j == numberOfRolls - 1)
+			{
+				continue;
+			}
+			thisTermMessage += ', ';
+		}
+		thisTermMessage += ')'
+
+		finalSum += termSum;
+		finalMessage += thisTermMessage
+		if(i == matches.length - 1)
+		{
+			continue;
+		}
+		finalMessage += " + ";
+	}
+	return '**Total:** ' + finalSum + ' = ' + finalMessage;
 };
-
-//useful regs
-//*
-
-
-// exports.roll = function (arg)
-// {
-// 	var numberOfRolls = parseInt(arg.substring(6,arg.toLowerCase().indexOf('d')));
-// 	var numberOfSides = parseInt(arg.substring(arg.toLowerCase().indexOf('d') + 1, arg.substring(arg.toLowerCase().indexOf('+') - 1)));
-// 	var sum = 0;
-// 	var total = '(';
-// 	console.log(numberOfRolls+'d'+numberOfSides);
-// 	console.log(arg.substring(arg.toLowerCase().indexOf('d') + 1, arg.substring(arg.toLowerCase().indexOf('+') - 1)));
-// 	for (var i = 0; i < numberOfRolls; i++)
-// 	{
-// 		var thisRoll = rand(1, numberOfSides);
-// 		sum += thisRoll;
-// 		total += thisRoll + '';
-// 		if(i == numberOfRolls - 1)
-// 		{
-// 			continue;
-// 		}
-// 		total += ', ';
-// 	}
-// 	total += ')';
-// 	return '**Total:** ' + sum + ' ' + total;
-// };
 
 exports.rps = function (arg)
 {
-	var botChoice = rand()
-
-	console.log(botChoice);
+	var botChoice = Math.floor(Math.random() * 3);
 	
-	arg = arg.toLowerCase().substring(5, arg.length + 1);
-	switch(arg)
+	switch(arg.toLowerCase().substring(5, arg.length + 1))
 	{
 		case 'rock':
 			switch(botChoice)
 			{
-				case '0':
-					return 'rock vs rock: It\'s a tie';
-					break;
-				case '1':
-					return 'rock vs paper: I win!'
-					break;
+				case 0:
+					return ':fist: & :fist:: It\'s a tie';
+				case 1:
+					return ':fist: & :newspaper:: You Lose!'
 				default:
-					return 'rock vs scissors: You win!';
-					break;
+					return ':fist: beats :scissors:: You win!';
 			}
 			break;
 		case 'paper':
 			switch(botChoice)
 			{
-				case '0':
-					return 'paper vs rock: You win!';
-					break;
-				case '1':
-					return 'paper vs paper: It\'s a tie';
-					break;
+				case 0:
+					return ':newspaper: beats :fist:: You win!';
+				case 1:
+					return ':newspaper: & :newspaper:: It\'s a tie';
 				default:
-					return 'paper vs scissors: I win!';
-					break;
+					return ':newspaper: & :scissors:: You Lose!';
 			}
-			break;
 		case 'scissors':
 			switch(botChoice)
 			{
-				case '0':
-					return 'scissors vs rock: I win!';
-					break;
-				case '1':
-					return 'scissors vs paper: You win!';
-					break;
+				case 0:
+					return ':scissors: & :fist:: You Lose!';
+				case 1:
+					return ':scissors: beat :newspaper:: You win!';
 				default:
-					return 'scissors vs scissors: It\'s a tie';
-					break;
+					return ':scissors: & :scissors:: It\'s a tie';
 			}
-	}
-}
-
-exports.uptime = function()
-{
-	var time = process.uptime();
-	var days = Math.floor(time/86400);
-	var hours = Math.floor(time/3600);
-	var minutes = Math.floor(time/60);
-	var seconds = Math.floor(time);
-	var result = seconds + ' secs ';
-	if(seconds > 60)
-	{
-		seconds %= 60;
-		minutes += Math.floor(seconds/60);
-	}
-	if(minutes > 60)
-	{
-		minutes %=60;
-		hours += Math.floor(minutes/60);
-	}
-	if(hours > 24)
-	{
-		hours %= 24;
-		days += Math.floor(hours/60);
-	}
-	if(minutes > 0)
-	{
-		result += (', ' + minutes + 'mins, ');
-		if(hours > 0)
-		{
-			result += (', ' + hours + 'hrs, ');
-			if(days > 0)
+		default:
+			switch(botChoice)
 			{
-				result += (', ' + days + 'days');
+				case 0:
+					return 'You wanna try that again?';
+				case 1:
+					return 'Nice try, asshole';
+				default:
+					return 'Retard alert!';
 			}
-		}
 	}
-	return result;
 }
 
-exports.rand = function(min, max)
+exports.age = function()
 {
-    return Math.floor(Math.random() * (max - min)) + min;
+	var time = process.uptime(); 
+
+	var seconds = Math.floor(time);
+	var minutes = 0;
+	var hours = 0;
+	var days = 0;
+	var years = 0;
+
+	while(seconds >= 60)
+	{
+		seconds -= 60;
+		minutes++;
+	}
+	while(minutes >= 60)
+	{
+		minutes -= 60;
+		hours++;
+	}
+	while(hours >= 24)
+	{
+		hours -= 24;
+		days++;
+	}
+	while(days >= 365)
+	{
+		days -= 365;
+		years++;
+	}
+
+	var message = '';
+	
+	if(years > 0)
+	{
+		message += years + ' year';
+		if(years != 1)
+			message += 's';
+		if(days > 0 || hours > 0 || minutes > 0 || seconds > 0)
+			message += ', ';
+	}
+	if (days > 0)
+	{
+		message += days + ' day';
+		if(days != 1)
+			message += 's';
+		if(hours > 0 || minutes > 0 || seconds > 0)
+			message += ', ';
+	}
+	if (hours > 0)
+	{
+		message += hours + ' hour';
+		if(hours != 1)
+			message += 's';
+		if(minutes > 0 || seconds > 0)
+			message += ', ';
+	}
+	if (minutes > 0)
+	{
+		message += minutes + ' minute';
+		if(minutes != 1)
+			message += 's';
+		if(seconds > 0)
+			message += ', ';
+	}
+	if (seconds > 0)
+	{
+		if(years > 0 || days > 0 || hours > 0 || minutes > 0)
+			message += "& ";
+		message += seconds + ' second'
+		if(seconds != 1)
+			message += 's';
+	}
+	return message;
+}
+
+exports.cpu = function()
+{
+	return process.cpuUsage();
+}
+
+exports.poll = function(arg)
+{
+	var pattern = /"(.*?)"/g;
+	arg = arg.toLowerCase().substring(6, indexOf(' '));
+	switch(arg)
+	{
+		case 'new':
+			var newPollName = arg.toLowerCase().substring(11, arg.length+1).match(pattern);
+			break;
+		case 'vote':
+			
+			break;
+		default:
+			return 'Unknown command.For help, type "!help poll"'	
+	}
+}
+
+exports.kill = function(arg)
+{
+	//TODO: kill
+}
+
+exports.pw = function(length)
+{
+	var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	var password = '';
+	for (var i = 0, n = charset.length; i < length; ++i)
+	{
+		password += charset.charAt(Math.floor(Math.random() * n));
+	}
+	return password;
+}
+
+function PollData(question, options)
+{
+	this.question = question;
+	this.options = options;
+}
+
+function PollOption()
+{
+
 }
--- skrimp.js
+++ skrimp.js
@@ -1,34 +1,66 @@
+const cmds = require('./cmds');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const cmds = require('./cmds');
 const token = 'NDQxMDI5NzEwMjMxODk2MDc1.Dcu0Uw.Xe5RmOQqu-99TFNbigkcGE4r94Y';
 const prefix = '!';
 
 client.on('ready', () =>
 {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg =>
 {
-    if (msg.content.charAt(0) === prefix)
-    {
-        switch (msg.content.toLowerCase().substring(1, msg.content.indexOf(' ', 1)))
-        {
-            case 'roll':
-                msg.reply(cmds.roll(msg.content));
-                break;
-            case 'rps':
-                msg.reply(cmds.rps(msg.content));
-                break;
-            case 'uptime':
-                msg.reply(cmds.uptime());
-                break;
-            default:
-                msg.reply('Unknown command. For help, type "!help"');
-                break;
-        }
-    }
+	if (msg.content.startsWith(prefix))
+	{
+		switch (msg.content.toLowerCase().substring(1, msg.content.indexOf(' ', 1)))
+		{
+			case 'roll':
+				msg.reply(cmds.roll(msg.content));
+				break;
+			case 'rps':
+				msg.reply(cmds.rps(msg.content));
+				break;
+			case 'age':
+				msg.reply(cmds.age());
+				break;
+			case 'kill':
+				msg.reply(cmds.kill());
+				break;
+			default:
+				msg.reply('Unknown command. For help, type "!help"');
+				break;
+		}
+	}
 });
 
 client.login(token);
+
+
+//!
+//  roll
+//      XdY
+//  rps
+//      hand
+//  uptime
+//		_
+//		seconds
+//		minutes
+//		hours
+//		days
+//  kill
+//      password
+//  poll
+//      new
+//          name
+//          option 1
+//          option 2
+//			...
+//		vote
+//			choice
+//	help
+//		roll
+//		rps
+//		uptime
+//		kill
+//		poll
